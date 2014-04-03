2ad7e6fa8e2cbd287392081c9c623a70ab3ee48a
Céline Souchet <celine.souchet@bonitasoft.com>
2014-04-03 15:54:43 +0200

    BS-7295 : [PERF] : 2 new indexes - Load Tests - 20000 instances - Case 00012456
    - Fix integration test

--- bonita-migration-db-filler-6.2.6/src/main/java/org/bonitasoft/migration/DatabaseChecker6_2_6.java
+++ bonita-migration-db-filler-6.2.6/src/main/java/org/bonitasoft/migration/DatabaseChecker6_2_6.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 214 BonitaSoft S.A.
+ * Copyright (C) 2014 BonitaSoft S.A.
  * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
  * This library is free software; you can redistribute it and/or modify it under the terms
  * of the GNU Lesser General Public License as published by the Free Software Foundation
@@ -15,9 +15,8 @@ package org.bonitasoft.migration;
 
 import org.junit.runner.JUnitCore;
 
+public class DatabaseChecker6_2_6 extends DatabaseChecker6_2_4 {
 
-public class DatabaseChecker6_2_6 extends SimpleDatabaseChecker6_2_4 {
-    
     public static void main(final String[] args) throws Exception {
         JUnitCore.main(DatabaseChecker6_2_6.class.getName());
     }
